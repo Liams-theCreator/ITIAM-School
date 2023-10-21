4061fa3d622c502a155483b03fe4e03c11141fee
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-21 23:28:31 +0100

    Answers

---
+++ TP 1 PHP/SOLUTION/ten.php
@@ -0,0 +1,21 @@
+<?php
+
+$int i =0;
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$result = $num1 + $num2;
+
+if ($result > 200)
+{
+    echo ("La somme est plus de 200");
+} 
+else 
+{
+    echo ("La somme est : $result");
+}
+
+for (; $i <=100 ; $i++) { 
+    echo("<br>Je suis stagiaire a ITIAM");
+}
+
+?>
